d1b0a9cebab6ba40af837b1d6ce59e4ba055b671
Garrett Goon <garrett361@gmail.com>
2024-12-12 20:04:55 +0000

    start test file

---
+++ test.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+
+class TestGraphs:
+    def test_get_graph(self) -> None:
+        graph_module = torch.fx.symbolic_trace(nn.Linear(8, 8))
+        print(graph_module.graph)
+        assert isinstance(graph_module.graph, torch.fx.graph.Graph)
+
+    def test_get_nodes(self) -> None:
+        graph_module = torch.fx.symbolic_trace(nn.Linear(8, 8))
+        nodes = [n for n in graph_module.graph.nodes]
+        print(nodes)
+        for n in nodes:
+            assert isinstance(n, torch.fx.node.Node)
+
+
+if __name__ == "__main__":
+    t = TestGraphs()
+    t.test_get_graph()
