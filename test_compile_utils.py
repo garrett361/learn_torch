80ab17db19a4ae15568c2c2ee3ea779e37eb5d52
Garrett Goon <garrett361@gmail.com>
2024-12-13 21:53:27 +0000

    utils test

--- test_compile_utils.py
+++ test_compile_utils.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 class TestGraphs:
     def test_get_graph(self) -> None:
         graph_module = torch.fx.symbolic_trace(nn.Linear(8, 8))
+        graph_module.print_readable()
         print(graph_module.graph)
         assert isinstance(graph_module.graph, torch.fx.graph.Graph)
 
