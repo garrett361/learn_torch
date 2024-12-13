42775a2a477dbeae8fb0ce106194512229b5b02f
Garrett Goon <garrett361@gmail.com>
2024-12-13 18:34:02 +0000

    test_graph_breaks.py

---
+++ test_graph_breaks.py
@@ -0,0 +1,26 @@
+import torch
+import utils
+from torch._dynamo.exc import Unsupported
+import pytest
+
+
+class TestBreaks:
+    def test_no_break(self) -> None:
+        def square(t: torch.Tensor) -> torch.Tensor:
+            return t**2
+
+        compiled = torch.compile(square, fullgraph=True)
+        inputs = torch.randn(1, device=utils.get_device())
+        out = compiled(inputs)
+        torch.testing.assert_close(out, inputs**2)
+
+    def test_break(self) -> None:
+        def square(t: torch.Tensor) -> torch.Tensor:
+            out = t**2
+            print(out)
+            return out
+
+        compiled = torch.compile(square, fullgraph=True)
+        inputs = torch.randn(1, device=utils.get_device())
+        with pytest.raises(Unsupported):
+            out = compiled(inputs)
