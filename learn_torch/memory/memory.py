93ae6447e72d5ac6d25be2980aeea7b4161b737b
Garrett Goon <garrett361@gmail.com>
2025-01-16 22:35:27 +0000

    CUDAMemoryConext updates

--- learn_torch/memory/memory.py
+++ learn_torch/memory/memory.py
@@ -3,24 +3,42 @@ from typing import Any, Union
 
 
 class CUDAMemContext:
-    def __init__(self, use_gib: bool = True) -> None:
+    def __init__(
+        self,
+        use_GiB: bool = True,
+        filter_patterns: tuple[str, ...] = ("allocated_bytes.all", "reserved_bytes.all"),
+        mem_only: bool = True,
+    ) -> None:
         # Ensure CUDA libraries are loaded:
         torch.cuda.current_blas_handle()
 
-        self.use_gib = use_gib
+        self.use_GiB = use_GiB
+        self.filter_patterns = filter_patterns
+        self.mem_only = mem_only
         self.before: dict[str, Union[int, float]] = {}
         self.after: dict[str, Union[int, float]] = {}
         self.delta: dict[str, Union[int, float]] = {}
 
     def _get_mem_dict(self) -> dict[str, Union[int, float]]:
-        return {
-            k: v if not self.use_gib else v / 2**30 for k, v in torch.cuda.memory_stats().items()
-        }
+        mem_dict = torch.cuda.memory_stats()
+        if self.filter_patterns:
+            mem_dict = {
+                k: v for k, v in mem_dict.items() if any(p in k for p in self.filter_patterns)
+            }
+        if self.mem_only:
+            mem_dict = {k: v for k, v in mem_dict.items() if "bytes" in k}
+        if self.use_GiB:
+            mem_dict = {
+                k.replace("bytes", "GiB"): v / 2**30 if "bytes" in k else v
+                for k, v in mem_dict.items()
+            }
+        return mem_dict
 
     def __enter__(self) -> "CUDAMemContext":
         self.before = self._get_mem_dict()
         return self
 
     def __exit__(self, *args: Any, **kwargs: Any) -> None:
+        torch.cuda.synchronize()
         self.after = self._get_mem_dict()
         self.delta = {k: v - self.before[k] for k, v in self.after.items()}
