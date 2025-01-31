2486956bf6d5c1773af08daa38ece5ed3f86fc91
Garrett Goon <garrett361@gmail.com>
2025-01-31 15:42:56 +0000

    update basic_fsdp2_graph

--- learn_torch/compile/basic_fsdp2_graph.py
+++ learn_torch/compile/basic_fsdp2_graph.py
@@ -3,7 +3,7 @@ import os
 
 import torch
 import torch.nn as nn
-from torch.distributed import destroy_process_group, init_process_group
+from torch.distributed import destroy_process_group, init_process_group, barrier
 from torch.distributed._composable.fsdp import fully_shard
 
 """
@@ -16,10 +16,10 @@ Trying to get the full graph to compile.
 
 
 class BasicModel(nn.Module):
-    def __init__(self, d_model: int, device: torch.device) -> None:
+    def __init__(self, d_model: int, device: torch.device, dtype: torch.dtype) -> None:
         super().__init__()
         self.d_model = d_model
-        self.lin0 = nn.Linear(d_model, d_model, bias=False, device=device)
+        self.lin0 = nn.Linear(d_model, d_model, bias=False, device=device, dtype=dtype)
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         outputs = self.lin0(inputs).relu()
@@ -27,8 +27,14 @@ class BasicModel(nn.Module):
 
 
 def set_torch_compile_flags() -> None:
+    if int(os.environ["RANK"]) == 0:
+        os.environ["TORCH_COMPILE_DEBUG"] = "1"
+        os.environ["TORCH_LOGS"] = "graph_breaks,aot_graphs,compiled_fwd_bwd"
+
     # From linsong, inherited from FSDP tests
     # https://github.com/pytorch/pytorch/blob/135a2d44830b2de1ed6714f52cc6a612406adb6d/test/distributed/_composable/fsdp/test_fully_shard_compile.py#L569-L570
+
+    # Requires torch >= ??
     torch._dynamo.config.skip_fsdp_hooks = False
     torch._dynamo.config.compiled_autograd = True
     torch._dynamo.config.inline_inbuilt_nn_modules = True
@@ -41,31 +47,44 @@ def set_torch_compile_flags() -> None:
         "raise_comms",
         "reorder_compute_for_overlap",
     ]
+    # Avoid AssertionError: BackwardState requires CompiledAutograd:
+    # https://pytorch.org/tutorials//intermediate/compiled_autograd_tutorial#basic-usage
+    torch._dynamo.config.compiled_autograd = True
 
 
 def main() -> None:
     set_torch_compile_flags()
     local_rank = int(os.environ["LOCAL_RANK"])
     device = torch.device(f"cuda:{local_rank}")
+    dtype = torch.bfloat16
     torch.cuda.set_device(device)
     init_process_group("nccl")
 
-    model = nn.Sequential(*(BasicModel(args.d_model, device) for _ in range(3)))
+    model = nn.Sequential(*(BasicModel(args.d_model, device, dtype) for _ in range(3)))
     for module in model.modules():
         if isinstance(module, BasicModel):
             fully_shard(module)
     fully_shard(model)
     if not local_rank:
         print(f"{model=}")
-        for name, param in model.named_parameters():
-            print(f"{name=}, {param=}")
 
-    compiled_model = torch.compile(model)
-    inputs = torch.randn(1, args.d_model, device=device)
+    # Apparently need to run the model once before compiling, for some reason. Maybe due to lazy
+    # behaviors?
 
-    outputs = compiled_model(inputs)
-    print(f"{local_rank=}, {outputs=}")
-    outputs.pow(2).mean().backward()
+    def fwd_bwd() -> None:
+        inputs = torch.randn(1, args.d_model, device=device, dtype=dtype)
+        outputs = model(inputs)
+        outputs.pow(2).mean().backward()
+
+    fwd_bwd()
+    barrier()
+    if not local_rank:
+        print("DONE non-compiled fwd + bwd")
+
+    compiled_fwd_bwd = torch.compile(fwd_bwd)
+    compiled_fwd_bwd()
+    if not local_rank:
+        print("DONE compiled fwd + bwd")
 
 
 if __name__ == "__main__":
@@ -78,26 +97,6 @@ if __name__ == "__main__":
         os.environ["TORCH_LOGS"] = "graph_breaks,aot_graphs"
 
     try:
-        local_rank = int(os.environ["LOCAL_RANK"])
-        device = torch.device(f"cuda:{local_rank}")
-        torch.cuda.set_device(device)
-        init_process_group("nccl")
-
-        model = nn.Sequential(*(BasicModel(args.d_model, device) for _ in range(3)))
-        for module in model.modules():
-            if isinstance(module, BasicModel):
-                fully_shard(module)
-        fully_shard(model)
-        if not local_rank:
-            print(f"{model=}")
-            for name, param in model.named_parameters():
-                print(f"{name=}, {param=}")
-
-        compiled_model = torch.compile(model)
-        inputs = torch.randn(1, args.d_model, device=device)
-
-        outputs = compiled_model(inputs)
-        print(f"{local_rank=}, {outputs=}")
-        outputs.pow(2).mean().backward()
+        main()
     finally:
         destroy_process_group()
