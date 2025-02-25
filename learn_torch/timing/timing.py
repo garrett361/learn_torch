88ae80298e99335566ac68901aeae1d6107e99de
Garrett Goon <garrett361@gmail.com>
2025-02-25 11:14:18 -0500

    more consistent names

--- learn_torch/timing/timing.py
+++ learn_torch/timing/timing.py
@@ -4,18 +4,18 @@ import torch
 class CUDATimer:
     def __init__(self) -> None:
         self._start_events: list[torch.cuda.Event] = []
-        self._end_events: list[torch.cuda.Event] = []
+        self._stop_events: list[torch.cuda.Event] = []
 
     def __enter__(self) -> "CUDATimer":
         start = torch.cuda.Event(enable_timing=True)
-        end = torch.cuda.Event(enable_timing=True)
+        stop = torch.cuda.Event(enable_timing=True)
         start.record()
         self._start_events.append(start)
-        self._end_events.append(stop)
+        self._stop_events.append(stop)
         return self
 
     def __exit__(self, *args, **kwargs) -> None:
-        self._end_events[-1].record()
+        self._stop_events[-1].record()
 
     def __len__(self) -> int:
         return len(self._start_events)
@@ -25,8 +25,8 @@ class CUDATimer:
             return [0.0]
         torch.cuda.synchronize()
         time_list_s = [
-            start.elapsed_time(end) / 1e3
-            for start, end in zip(self._start_events, self._end_events)
+            start.elapsed_time(stop) / 1e3
+            for start, stop in zip(self._start_events, self._stop_events)
         ]
         return time_list_s
 
@@ -39,4 +39,4 @@ class CUDATimer:
 
     def reset(self) -> None:
         self._start_events.clear()
-        self._end_events.clear()
+        self._stop_events.clear()
