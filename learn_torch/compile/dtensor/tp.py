095ecac02014005bd070f9a6ea7b6af539ad7c61
Garrett Goon <garrett361@gmail.com>
2025-03-19 20:30:48 +0000

    basic DTensor TP compile

---
+++ learn_torch/compile/dtensor/tp.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+from torch.distributed.device_mesh import init_device_mesh
+from torch.distributed.tensor.parallel import ColwiseParallel, RowwiseParallel, parallelize_module
+
+from dtest import DTest
+
+
+class MLP(nn.Module):
+    def __init__(self, device, d_model: int = 32) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.lin0 = nn.Linear(self.d_model, self.d_model, bias=False, device=device)
+        self.lin1 = nn.Linear(self.d_model, self.d_model, bias=False, device=device)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        return self.lin1(self.lin0(inputs).relu())
+
+
+class TestTP(DTest):
+    def test(self) -> None:
+        torch.manual_seed(42)
+        model = MLP(self.device)
+        tp_mesh = init_device_mesh("cuda", (self.world_size,))
+        model = parallelize_module(
+            model, tp_mesh, {"lin0": ColwiseParallel(), "lin1": RowwiseParallel()}
+        )
+        inputs = torch.randn(1, model.d_model)
+        outputs = model(inputs)
+
+    def test_compiled(self) -> None:
+        torch.manual_seed(42)
+        model = MLP(self.device)
+        tp_mesh = init_device_mesh("cuda", (self.world_size,))
+        model = parallelize_module(
+            model, tp_mesh, {"lin0": ColwiseParallel(), "lin1": RowwiseParallel()}
+        )
+        model_compiled = torch.compile(model, backend="inductor", fullgraph=True)
+        inputs = torch.randn(1, model.d_model)
+        outputs = model_compiled(inputs)
