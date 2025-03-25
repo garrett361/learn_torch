7751c65de491407ad2cd9156b8d6e7ab5d416bd7
Garrett Goon <garrett361@gmail.com>
2025-03-25 17:11:05 +0000

    all_to_all cleanup

--- learn_torch/distributed/all_to_all.py
+++ learn_torch/distributed/all_to_all.py
@@ -27,16 +27,16 @@ class TestAllToAll(DTest):
         # input_split_sizes determines where chunk boundaries on the inputs are and how many
         # elements to send to each rank: send_numel_to_rank[r] = input_split_sizes[r],
         # schematically, with
-        # Must have sum(input_split_sizes) == inputs.numel().
+        # Must have sum(input_split_sizes) == inputs.shape[0].
         input_split_sizes = [
-            t.numel() if ((rank + 1) % self.world_size) == self.rank else 0
+            t.shape[0] if ((rank + 1) % self.world_size) == self.rank else 0
             for rank in range(self.world_size)
         ]
         # Seems like output_split_sizes is the conjugate, determining how many elements should be
         # received from which ranks: numel_from_rank[r] = output_split_sizes[r], schematically.
-        # Must have sum(output_split_sizes) == outputs.numel().
+        # Must have sum(output_split_sizes) == outputs.shape[0].
         output_split_sizes = [
-            t.numel() if ((rank - 1) % self.world_size) == self.rank else 0
+            t.shape[0] if ((rank - 1) % self.world_size) == self.rank else 0
             for rank in range(self.world_size)
         ]
         dist.all_to_all_single(out, t, output_split_sizes, input_split_sizes)
@@ -49,10 +49,10 @@ class TestAllToAll(DTest):
         Build a gather to rank zero.
         """
         t = torch.tensor([self.rank], device=self.device, dtype=self.dtype)
-        input_split_sizes = [t.numel()] + [0 for _ in range(self.world_size - 1)]
+        input_split_sizes = [t.shape[0]] + [0 for _ in range(self.world_size - 1)]
         if not self.rank:
             out = torch.empty(self.world_size, device=self.device, dtype=self.dtype)
-            output_split_sizes = [t.numel() for _ in range(self.world_size)]
+            output_split_sizes = [t.shape[0] for _ in range(self.world_size)]
         else:
             out = torch.empty(0, device=self.device, dtype=self.dtype)
             output_split_sizes = [0 for _ in range(self.world_size)]
@@ -62,26 +62,27 @@ class TestAllToAll(DTest):
                 out, torch.arange(self.world_size, device=self.device, dtype=self.dtype)
             )
         else:
-            assert not out.numel()
+            assert not out.shape[0]
 
     def test_permute_autograd(self) -> None:
         """
-        Use the {input,output}_split_sizes args to pass tensors around.
+        Use the {input,output}_split_sizes args to pass tensors around. Passes from higher rank to
+        lower rank.
         """
         t = torch.tensor([self.rank], device=self.device)
         # input_split_sizes determines where chunk boundaries on the inputs are and how many
         # elements to send to each rank: send_numel_to_rank[r] = input_split_sizes[r],
         # schematically, with
-        # Must have sum(input_split_sizes) == inputs.numel().
+        # Must have sum(input_split_sizes) == inputs.shape[0].
         input_split_sizes = [
-            t.numel() if ((rank + 1) % self.world_size) == self.rank else 0
+            t.shape[0] if ((rank + 1) % self.world_size) == self.rank else 0
             for rank in range(self.world_size)
         ]
         # Seems like output_split_sizes is the conjugate, determining how many elements should be
         # received from which ranks: numel_from_rank[r] = output_split_sizes[r], schematically.
-        # Must have sum(output_split_sizes) == outputs.numel().
+        # Must have sum(output_split_sizes) == outputs.shape[0].
         output_split_sizes = [
-            t.numel() if ((rank - 1) % self.world_size) == self.rank else 0
+            t.shape[0] if ((rank - 1) % self.world_size) == self.rank else 0
             for rank in range(self.world_size)
         ]
         out = funcol.all_to_all_single_autograd(
