062f2f33950a3419d7da49b73558ac3957f99123
Garrett Goon <goon@ibm.com>
2025-07-15 10:43:20 -0400

    more wandb fiddling

--- learn_torch/wandb/wandb_multi_proc_reporting.py
+++ learn_torch/wandb/wandb_multi_proc_reporting.py
@@ -7,18 +7,27 @@ import torch.distributed as dist
 import wandb
 
 
-def main(project_name, run_id, rank):
+def main(project_name, run_id, rank, local_rank):
     x_primary = rank == 0
     x_label = f"rank_{rank}"
+    # https://wandb.ai/dimaduev/simple-cnn-ddp/reports/Distributed-Training-with-Shared-Mode--VmlldzoxMTI0NTE1NA
+    settings = wandb.Settings(
+        x_label=x_label,
+        mode="shared",
+        x_stats_gpu_device_ids=[local_rank],
+        x_primary=x_primary,
+    )
+    if rank != 0:
+        # Do not upload wandb files except console logs.
+        settings.x_primary = False
+        # Do not change the state of the run on run.finish().
+        settings.x_update_finish_state = False
     run = wandb.init(
         project=project_name,
-        settings=wandb.Settings(
-            x_label=x_label,
-            mode="shared",
-            x_primary=x_primary,
-        ),
+        settings=settings,
         id=run_id,
     )
+
     print(f"[{rank=}]: {x_label=} {x_primary=} {run_id=} {run.id=}")
 
     # Log the rank as a wandb data point
@@ -26,7 +35,8 @@ def main(project_name, run_id, rank):
     for step in range(1, 11):
         stats = {f"stat_on_{rank}": random.random(), "loss": rank}
 
-        wandb.log(stats, step=step)
+        # wandb.log(stats, step=step)
+        wandb.log(stats)
 
 
 def parse_args():
@@ -47,6 +57,6 @@ if __name__ == "__main__":
     torch.cuda.set_device(device)
     try:
         dist.init_process_group(backend="nccl", rank=RANK, world_size=WORLD_SIZE, device_id=device)
-        main(args.project_name, args.run_id, RANK)
+        main(args.project_name, args.run_id, RANK, LOCAL_RANK)
     finally:
         dist.destroy_process_group()
